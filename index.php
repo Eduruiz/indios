ec13fc7acc6fdeb57897f2bb9ce11c1e4abd18e3
Edu Ruiz <contato@eduruiz.com.br>
2014-03-15 22:02:33 -0300

    Home finalizada
    Home page estática finalizada.

--- index.php
+++ index.php
@@ -12,31 +12,125 @@
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/flexslider.css">
         <link rel="stylesheet" href="css/main.css">
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+            <p class="browsehappy">Você está usando um navegador <strong>antigo!</strong>. Por favor <a href="http://browsehappy.com/">atualize seu navegador</a> para melhor experiência com o site.</p>
         <![endif]-->
 
-        <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <!-- Main Content -->
+        <header>
+            <div class="sixteen columns">
+                <a href="home" class="ir logo">
+                    <h1>Índios Nativos Valvulados</h1>
+                </a>
+                
+                <nav class="menu">
+                    <ul>
+                        <li><a href="home" class="active">Início</a></li>
+                        <li><a href="">Para Ouvir</a></li>
+                        <li><a href="lifestyle">Lifestyle</a></li>
+                        <li><a href="sobre_nos">Sobre Nós</a></li>
+                        <li><a href="contato">Contato</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <div class="slider-container">
+            <div class="container">
+                <section class="slider">
+                    <div class="flexslider">
+                        <ul class="slides">
+                            <li>
+                                <img src="images/slider/1.jpg" />        
+                            </li>
+                            <li>
+                                <img src="images/slider/1.jpg" />        
+                            </li>
+                            <li>
+                                <img src="images/slider/1.jpg" />        
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <section class="seven columns agenda">
+                    <h2>Agenda</h2>
+                    <a href="agenda">25/04 - Festival Abril pro Rock - Olinda - PE</a>
+                    <div class="bar-ornament"></div>
+                    <a href="agenda">25/04 - Festival Abril pro Rock - Olinda - PE</a>
+                    <div class="bar-ornament"></div>
+                    <a href="agenda">25/04 - Festival Abril pro Rock - Olinda - PE</a>
+                    <div class="bar-ornament"></div>
+                    <a href="agenda">25/04 - Festival Abril pro Rock - Olinda - PE</a>
+                    <div class="bar-ornament"></div>
+                </section>
+                <section class="nine columns soundcloud">
+                    <h2 class="inverse">
+                        Para Ouvir
+                    </h2>
+                    <iframe scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/22869900&amp;color=ff9900&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true"></iframe>
+                </section>
+            </div>
+        </div>
+        
+        <div class="divider"></div>
+        
+        <div class="container">
+            <div class="row">
+                <section class="seven columns">
+                    <h2>Entre para a tribo</h2>
+                    <div id="fb-root"></div>
+                    <div class="fb-like-box" data-href="https://www.facebook.com/indiosnativosvalvulados" data-width="100%" data-height="350" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="true" data-show-border="true"></div>
+                </section>
+                <section class="nine columns video">
+                    <h2 class="inverse">Vídeos da Tribo</h2>
+                    <iframe src="//www.youtube.com/embed/fZ50X8oi-kg" frameborder="0" allowfullscreen></iframe>
+                </section>
+            </div>
+        </div>
+
+        <footer>
+            <div class="container">
+                <div class="eight columns">
+                    <span>Índios Nativos Valvulados &copy; todos os direitos reservados</span>
+                </div>
+                <div class="eight columns midias">
+                    <ul>
+                        <li>nossas mídias</li> 
+                        <li><a href="http://instagram.com/indiosnativosvalvulados"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="http://www.flickr.com/photos/arteinv"><i class="fa fa-flickr"></i></a></li>
+                        <li><a href="https://www.youtube.com/user/ArteINV"><i class="fa fa-youtube-square"></i></a></li>
+                        <li><a href="https://www.facebook.com/indiosnativosvalvulados"><i class="fa fa-facebook-square"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+        <div class="ornament"></div>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        
+        <script src="js/min/plugins-ck.js"></script>
+        <script src="js/min/main-ck.js"></script>
+        
         <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X');ga('send','pageview');
+          $(window).load(function() {
+            $('.flexslider').flexslider({
+                animation: "slide",
+                controlNav: false,
+                prevText: "",
+                nextText: "",
+                useCSS: false,
+            });
+          });
         </script>
     </body>
 </html>
